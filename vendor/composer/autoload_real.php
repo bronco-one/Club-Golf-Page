8d5d9a7faf9a7dac9dd80d8f536b9ba07497f4fe
Zdzisław Bronikowski <bronco-one@users.noreply.github.com>
2025-08-31 14:46:20 +0200

    iport env

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit1e7afe380bc4d3c2b926980f9c0e0bd6
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit1e7afe380bc4d3c2b926980f9c0e0bd6::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
